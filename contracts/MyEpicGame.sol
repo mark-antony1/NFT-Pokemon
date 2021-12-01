75472903da4e74e0e39ea96030d9f7dda8ef8cf9
Mark Antony <jchristian50@gmail.com>
2021-12-01 10:33:00 -0800

    implement basics of VRF consumer code

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 // NFT contract to inherit from.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
 // Helper functions OpenZeppelin provides.
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -14,7 +15,7 @@ import "hardhat/console.sol";
 import "./libraries/Base64.sol";
 
 // Our contract inherits from ERC721, which is the standard NFT contract!
-contract MyEpicGame is ERC721 {
+contract MyEpicGame is ERC721, VRFConsumerBase {
 
   struct CharacterAttributes {
     uint characterIndex;
@@ -78,12 +79,20 @@ contract MyEpicGame is ERC721 {
 	// A mapping from an the NFTs tokenId => address. I can retrieve an owner based on a pokemon ID.
   mapping(uint256 => address) public pokemonToOwner;
 
-	event AttackComplete(uint newBossHp, uint newPlayerHp, uint256 tokenId);
-	event CharacterNFTMinted(address sender, uint256 tokenId, uint256 characterIndex);
-	event PokemonRevived(uint256 tokenId, uint newPokemonHealth);
+	//stores the results of the random generator to the tokenid of the attacking pokemon
+	mapping(bytes32 => uint256) private requestToPokemon;
 
 	// A mapping from an the address => list of NFT tokenIds. I can retrieve which person owns which pokemon.
   mapping(address => uint256[]) public ownerToPokemons;
+
+	//keyHash identifies the chainlink oracle we will get random numbers from
+	//fee identifies the fee in LINK we will pay to the oracles
+	bytes32 private keyHash;
+	uint256 private fee;
+
+	event AttackComplete(uint newBossHp, uint newPlayerHp, uint256 tokenId);
+	event CharacterNFTMinted(address sender, uint256 tokenId, uint256 characterIndex);
+	event PokemonRevived(uint256 tokenId, uint newPokemonHealth);
 
   // Data passed in to the contract when it's first created initializing the characters.
   // We're going to actually pass these values in from from run.js.
@@ -95,9 +104,13 @@ contract MyEpicGame is ERC721 {
 		uint[] memory characterDefense,
 		string[] memory characterMoveName,
 		uint[] memory characterCritChance,
-		BigBossArguments memory bossData
+		BigBossArguments memory bossData,
+		address _vrfCoordinator,
+		address _link,
+		bytes32 _keyHash
   )
 		ERC721("Pokemon", "POKEMON")
+		VRFConsumerBase(_vrfCoordinator, _link)
   {
 		bigBoss = BigBoss({
 			name: bossData.name,
@@ -134,6 +147,8 @@ contract MyEpicGame is ERC721 {
     }
 		// I increment tokenIds here so that my first NFT has an ID of 1.
 		_tokenIds.increment();
+		keyHash = _keyHash;
+		fee = 0.1 * 10**18; // 0.1 LINK
   }
 
   // Users would be able to hit this function and get their NFT based on the
@@ -169,6 +184,12 @@ contract MyEpicGame is ERC721 {
     // Increment the tokenId for the next person that uses it.
     _tokenIds.increment();
 		emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
+	}
+
+	function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+		uint256 d100Value = (randomness%100)+1;
+    uint256 tokenId = requestToPokemon[requestId];
+		attackBossV2(tokenId, d100Value);
 	}
 
 	function tokenURI(uint256 _tokenId) public view override returns (string memory) {
@@ -224,6 +245,36 @@ contract MyEpicGame is ERC721 {
 			pokemon.hp = 0;
 		} else {
 			pokemon.hp = pokemon.hp - bigBoss.attackDamage;
+		}
+
+		console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
+  	console.log("Boss attacked player's pokemon. New pokemon hp: %s\n", pokemon.hp);
+		emit AttackComplete(bigBoss.hp, pokemon.hp, _tokenId);
+	}
+
+		function attackBossV2(uint256 _tokenId, uint256 _randomness) public {
+		address owner = pokemonToOwner[_tokenId];
+		require(msg.sender == owner);
+		CharacterAttributes storage pokemon = pokemonAttributes[_tokenId];
+		console.log("\nPlayer w/ character %s about to attack. Has %s HP and %s AD", pokemon.name, pokemon.hp, pokemon.attackDamage);
+		console.log("Boss %s has %s HP and %s AD", bigBoss.name, bigBoss.hp, bigBoss.attackDamage);
+		require (pokemon.hp > 0, "Error: pokemon is out of HP");
+		require (bigBoss.hp > 0, "Error: Mewtwo is out of HP");
+
+		if (bigBoss.hp < pokemon.attackDamage) {
+			bigBoss.hp = 0;
+		} else {
+			bigBoss.hp = bigBoss.hp - pokemon.attackDamage;
+		}
+
+		if (pokemon.hp < bigBoss.attackDamage) {
+			pokemon.hp = 0;
+		} else {
+			pokemon.hp = pokemon.hp - bigBoss.attackDamage;
+		}
+
+		if (_randomness <= pokemon.critChance) {
+			console.log("CRITICAL HIT for ", pokemon.name, " for",  pokemon.attackDamage);
 		}
 
 		console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
